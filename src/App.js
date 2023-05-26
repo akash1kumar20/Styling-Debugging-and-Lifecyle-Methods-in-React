b24cc00fb218556177db9286fbbc2cdf15533323
akash1kumar20 <127503323+akash1kumar20@users.noreply.github.com>
2023-05-26 13:19:08 +0530

    JSX limitation and solution

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ function App() {
       ];
     });
   };
+  // UserData doesn't have the access of data which we get from the UserForm component, if it  no access, it can show that data on the screen, App component is the closet component to UserData and UserForm component and have access for both the component, so with the the help of App, we make a connection b/w UserData and UserForm component.
 
   return (
-    <div>
+    <React.Fragment>
       <UserForm onAddUser={addUserHandler} />
+      {/* onAddUser is poniting to the addUserHandler function and we use onAddUser function in UserForm to store the data. */}
       <UserData users={usersList} />
-    </div>
+      {/* users give us the data in the array from which can we use in the UserData component and usersList is forwading to the component */}
+    </React.Fragment>
   );
 }
 
--- src/Components/Form/UserForm.js
+++ src/Components/Form/UserForm.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 import classes from "./UserForm.module.css";
+import Wrapper from "../Helpers/Wrapper";
 // for CSS module we're importing our CSS file in that, for non-module, we don't have to use classes form.
 import Button from "../UI/Button";
-function UserForm() {
+function UserForm(props) {
   const [error, setError] = useState();
+  //starting value is undefined, that's why we don't pass anything in the ().
   const [userName, setUserName] = useState("");
   const userNameSubmit = (event) => {
     setUserName(event.target.value);
@@ -23,31 +25,38 @@ function UserForm() {
         message: "Please enter a valid name and age (non-empty values).",
       });
       return;
+      //when there is a invalid data, we're not just going back, we're showing a message too.
     }
     if (+userAge < 1) {
+      //to convert from string to number, we use +userAge
       setError({
         title: "Invalid age",
         message: "Please enter a valid age (> 0).",
       });
       return;
     }
+    props.onAddUser(userName, userAge);
+    //forwading every data we get from the user to App component, and from that making a connection to the UserData component, for display.
     console.log("User Name is", userName, "User Age is", userAge);
     setUserName("");
     setUserAge("");
   };
   const errorHandler = () => {
     setError(null);
+    //this funcion is used to clear the message from the screen when we click on the okay button or anywhere on the screen.
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
           message={error.message}
           onConfirm={errorHandler}
+          //onConfirm function is describe the ErrorModal Component.
         />
       )}
+      {/* if don't get any error, then the below part will get exceuted. */}
       <Card className={classes.input}>
         <form onSubmit={userDetails}>
           <div>
@@ -72,7 +81,7 @@ function UserForm() {
           {/* Here Button is our custom component, that's why we use capital letter syntax, otherwise we use small letter.  */}
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 }
 export default UserForm;
